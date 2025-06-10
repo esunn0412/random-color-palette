2363dbb3369ad1af0d78c682e28bd34805920cb0
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-11 00:15:34 +0900

    footer added

---
+++ components/feature/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center border-t">
+      <div className="container flex h-14 items-center justify-center">
+        <p className="text-muted-foreground text-sm">
+          Built with Next.js, Tailwind CSS and shadcn/ui by Taeeun Kim
+        </p>
+      </div>
+    </footer>
+  );
+}
