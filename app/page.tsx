08662272e23c2b626fa68ab03c45cd114de8ec0f
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-10 22:11:24 +0900

    play with colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
 import { ThemeToggle } from '@/components/feature/themeToggle';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="container mx-auto flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">A Random Color Palette Generator</h1>
+    <main className="relative container flex min-h-screen min-w-screen flex-col items-center justify-center caret-transparent">
+      <div className="absolute top-4 right-4 z-10 cursor-pointer">
+        <ThemeToggle />
+      </div>
+
+      <h1 className="text-primary text-4xl font-bold">A Random Color Palette Generator</h1>
       <p className="text-muted-foreground mt-4 text-lg">
         Click the button below to generate a new palette!
       </p>
 
-      <div className="mt-4 flex flex-col items-center">
-        <ThemeToggle />
+      <div className="mt-4 flex items-center">
+        <Button variant="outline" className="dark:border-primary/30 cursor-pointer">
+          Generate
+        </Button>
       </div>
     </main>
   );
